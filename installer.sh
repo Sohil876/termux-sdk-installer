cdbce01281fed810902a9e13b9cd77a71d3689f0
Sohil876 <sohil876@gmail.com>
2023-08-11 14:49:24 +0530

    Update for new manifest, fix installer, format code, fix bugs

--- installer.sh
+++ installer.sh
@@ -4,39 +4,42 @@
 #
 
 # Vars
-install_dir=${HOME}
-manifest_url="https://raw.githubusercontent.com/itsaky/androidide-build-tools/main/manifest.json"
+install_dir="${HOME}"
+manifest_url="https://raw.githubusercontent.com/AndroidIDEOfficial/androidide-tools/main/manifest.json"
 manifest="${PWD}/manifest.json"
 # DO NOT CHANGE THESE!
 CURRENT_SHELL="${SHELL##*/}"
-CURRENT_DIR=${PWD}
-arch=$(dpkg --print-architecture)
+CURRENT_DIR="${PWD}"
+arch="$(dpkg --print-architecture)"
 
 # Color Codes
-red='\e[0;31m'             # Red
-green='\e[0;32m'        # Green
-cyan='\e[0;36m'          # Cyan
-white='\e[0;37m'        # White
-nocol='\033[0m'         # Default
+red="\e[0;31m"          # Red
+green="\e[0;32m"        # Green
+cyan="\e[0;36m"         # Cyan
+white="\e[0;37m"        # White
+nocol="\033[0m"         # Default
 
 # Functions
 banner() {
-  echo -e "${green}------------------------------------------------"
-  echo "          Termux Android SDK Installer"
-  echo -e "               ${green}(${cyan}By ${green}-:- ${white}Sohil876${green})${nocol}"
-  echo -e "${green}------------------------------------------------${nocol}"
-  echo ""
+  echo -e "
+${green}------------------------------------------------
+
+Termux Android SDK Installer${nocol}:
+https://github.com/Sohil876/termux-sdk-installer
+
+${green}------------------------------------------------${nocol}
+"
 }
 
 download_and_extract() {
   # Name shown in echo
-  name=${1}
+  name="${1}"
   # URL to download from
-  url=${2}
+  url="${2}"
   # Directory in which the downloaded archive will be extracted
-  dir=${3}
+  dir="${3}"
   # Destination path for downloading the file
-  dest=${4}
+  dest="${4}"
 
   cd ${dir}
   do_download=true
@@ -80,12 +83,18 @@ gen_data() {
     exit 1
   fi
   curl --silent -L -o ${manifest} ${manifest_url}
-  tmp=$(cat ${manifest} | jq -n "[inputs[] | keys[]]" | jq -r ".[0]")
-  sdk_m_version="${tmp:1}"
-  sdk_version="${sdk_m_version//_/.}"
-  sdk_url_string=".android_sdk._${sdk_m_version}.${arch}.sdk"
-  sdk_url=$(cat ${manifest} | jq -r ${sdk_url_string})
-  cmdline_url=$(cat ${manifest} | jq -r ".android_sdk.cmdline_tools")
+  sdk_url=$(cat ${manifest} | jq -r .android_sdk)
+  sdk_file=${sdk_url##*/}
+  sdk_m_version=($(cat ${manifest} | jq .build_tools.${arch} | jq -r 'keys_unsorted[]'))
+  sdk_m_version=${sdk_m_version[0]}
+  sdk_version=${sdk_m_version:1}
+  sdk_version="${sdk_version//_/.}"
+  build_tools_url=($(cat ${manifest} | jq .build_tools.${arch} | jq -r .${sdk_m_version}))
+  build_tools_file=${build_tools_url##*/}
+  cmdline_tools_url=$(cat ${manifest} | jq -r .cmdline_tools)
+  cmdline_tools_file=${cmdline_tools_url##*/}
+  platform_tools_url=($(cat ${manifest} | jq .platform_tools.${arch} | jq -r .${sdk_m_version}))
+  platform_tools_file=${platform_tools_url##*/}
   rm ${manifest}
 }
 
@@ -103,9 +112,11 @@ info() {
   echo -e "${green}Active Shell:${nocol} ${CURRENT_SHELL}"
   echo -e "${green}Arch:${nocol} ${arch}"
   echo -e "${green}JDK:${nocol} OpenJDK 17"
-  echo -e "${green}SDK version:${nocol} v${sdk_version}"
+  echo -e "${green}SDK/Tools version:${nocol} v${sdk_version}"
   echo -e "${green}SDK url:${nocol} ${sdk_url}"
-  echo -e "${green}SDK cmdline tools url:${nocol} ${cmdline_url}"
+  echo -e "${green}Build tools url:${nocol} ${build_tools_url}"
+  echo -e "${green}Commandline tools url:${nocol} ${cmdline_tools_url}"
+  echo -e "${green}Platform tools url:${nocol} ${platform_tools_url}"
   echo -e "${green}SDK from:${nocol} https://github.com/itsaky"
 }
 
@@ -113,7 +124,7 @@ install() {
   echo ""
   gen_data
   echo -e "${green}Installing dependencies...${nocol}"
-  apt update
+  pkg update
   pkg install curl wget termux-tools jq tar -y
   echo -e "${red}!${nocol}${green}This will download ~400MB size files and will take ~600MB space on disk.${nocol}"
   echo -e "Continue? ([${green}y${nocol}]es/[${red}N${nocol}]o): "
@@ -125,10 +136,14 @@ install() {
   echo -e "${green}Installing jdk...${nocol}"
   pkg install openjdk-17 -y
   echo -e "${green}Downloading sdk files...${nocol}"
-  # Download and extract the Android SDK
-  download_and_extract "Android SDK" ${sdk_url} ${install_dir} "${install_dir}/android-sdk.tar.xz"
+  # Download and extract the android SDK
+  download_and_extract "Android SDK" ${sdk_url} ${install_dir} "${install_dir}/${sdk_file}"
+  # Download and extract build tools
+  download_and_extract "Build tools" ${build_tools_url} "${install_dir}/android-sdk" "${install_dir}/${build_tools_file}"
   # Download and extract cmdline tools
-  download_and_extract "Command line tools" ${cmdline_url} "${install_dir}/android-sdk" "${install_dir}/cmdline_tools.tar.xz"
+  download_and_extract "Command line tools" ${cmdline_tools_url} "${install_dir}/android-sdk" "${install_dir}/${cmdline_tools_file}"
+  # Download and extract platform tools
+  download_and_extract "Platform tools" ${platform_tools_url} "${install_dir}/android-sdk" "${install_dir}/${platform_tools_file}"
   # Setting env vars
   echo -e "${green}Setting up env vars...${nocol}"
   if [[ "${CURRENT_SHELL}" == "bash" ]]; then
@@ -139,32 +154,33 @@ install() {
     unsupported_shell_used=true
     echo -e "${red}Unsupported shell!${nocol}"
     echo -e "${green}You will need to manually export env vars JAVA_HOME, ANDROID_SDK_ROOT and ANDROID_HOME on every session to use sdk, or add them to your shell profile manually:${nocol}"
-    echo "export JAVA_HOME=${PREFIX}/opt/openjdk-17"
-    echo "export ANDROID_SDK_ROOT=${install_dir}/android-sdk"
-    echo "export ANDROID_HOME=${install_dir}/android-sdk"
+    echo 'export JAVA_HOME=${PREFIX}/opt/openjdk-17'
+    echo 'export ANDROID_SDK_ROOT=${HOME}/android-sdk'
+    echo 'export ANDROID_HOME=${HOME}/android-sdk'
     echo -e "${green}Also do the same for sdk and jdk bin locations:${nocol}"
-    echo "export PATH=${PREFIX}/opt/openjdk/bin:${install_dir}/android-sdk/cmdline-tools/latest/bin:${PATH}"
+    echo 'export PATH=${PREFIX}/opt/openjdk/bin:${HOME}/android-sdk/cmdline-tools/latest/bin:${PATH}'
   fi
   if [[ -z "${unsupported_shell_used}" ]]; then
     if [[ -z "${JAVA_HOME}" ]]; then
-      echo "JAVA_HOME=${PREFIX}/opt/openjdk" >> ${shell_profile}
+      echo '\nexport JAVA_HOME=${PREFIX}/opt/openjdk\n' >> ${shell_profile}
+      echo '\nexport PATH=${PREFIX}/opt/openjdk/bin:${PATH}\n' >> ${shell_profile}
     else
       echo "JAVA_HOME is already set to: ${JAVA_HOME}"
       echo "Check if the path is correct, it should be: ${PREFIX}/opt/openjdk"
     fi
     if [[ -z "${ANDROID_SDK_ROOT}" ]]; then
-      echo "ANDROID_SDK_ROOT=${install_dir}/android-sdk" >> ${shell_profile}
+      echo '\nexport ANDROID_SDK_ROOT=${HOME}/android-sdk\n' >> ${shell_profile}
     else
       echo "ANDROID_SDK_ROOT is already set to: ${ANDROID_SDK_ROOT}"
       echo "Check if the path is correct, it should be: ${install_dir}/android-sdk"
     fi
     if [[ -z "${ANDROID_HOME}" ]]; then
-      echo "ANDROID_HOME=${install_dir}/android-sdk" >> ${shell_profile}
+      echo '\nexport ANDROID_HOME=${HOME}/android-sdk\n' >> ${shell_profile}
     else
       echo "ANDROID_HOME is already set to: ${ANDROID_HOME}"
       echo "Check if the path is correct, it should be: ${install_dir}/android-sdk"
     fi
-    echo "export PATH=${PREFIX}/opt/openjdk/bin:${install_dir}/android-sdk/cmdline-tools/latest/bin:${PATH}" >> ${shell_profile}
+    echo '\nexport PATH=${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/platform-tools:${PATH}\n' >> ${shell_profile}
   fi
   apt clean
 }
